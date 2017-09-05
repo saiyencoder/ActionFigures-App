9f72e121230990672021bf6a618584b7705197e4
saiyencoder <jsantizo20@gmail.com>
2017-09-05 18:25:23 -0500

    new&create

--- app/controllers/action_figures_controller.rb
+++ app/controllers/action_figures_controller.rb
@@ -1,12 +1,36 @@
 class ActionFiguresController < ApplicationController
-  def action_figures_action
-    @action_figure = ActionFigure.first
-    render 'action_figures_page.html.erb'
+  # def action_figures_action
+  #   @action_figure = ActionFigure.first
+  #   render 'action_figures_page.html.erb'
+  # end
+
+  # def all_action_figures_action
+  #   @action_figures = ActionFigure.all
+  #   render 'all_action_figures_page.html.erb'
+  # end
+
+  def index
+    @action_figures = ActionFigure.all
+
   end
 
-  def all_action_figures_action
-    @action_figures = ActionFigure.all
-    render 'all_action_figures_page.html.erb'
+  def new
+    
+  end
+
+  def create
+    action_figure = ActionFigure.new(
+                                    name: params[:name],
+                                    description: params[:description],
+                                    image: params[:image],
+                                    price: params[:price]
+                                    )
+    action_figure.save
+  end
+
+
+  def show
+    @action_figure = ActionFigure.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  get '/action_figures_url' => 'action_figures#action_figures_action'
-  get '/all_action_figures_url' => 'action_figures#all_action_figures_action'
+  # get '/action_figures_url' => 'action_figures#action_figures_action'
+  # get '/all_action_figures_url' => 'action_figures#all_action_figures_action'
+  get '/' => 'action_figures#index'
+  get '/action_figures' => 'action_figures#index'
+
+  get '/action_figures/new' => 'action_figures#new'
+  post '/action_figures' => 'action_figures#create'
+
+  get '/action_figures/:id' => 'action_figures#show'
 end
