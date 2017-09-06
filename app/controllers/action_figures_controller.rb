8a36d958f09b382f34f63a6f12e82b94803a44f2
saiyencoder <jsantizo20@gmail.com>
2017-09-05 21:26:44 -0500

    destroy%flash

--- app/controllers/action_figures_controller.rb
+++ app/controllers/action_figures_controller.rb
@@ -26,11 +26,39 @@ class ActionFiguresController < ApplicationController
                                     price: params[:price]
                                     )
     action_figure.save
+    flash[:success] = "Action Figure Created"
+    redirect_to "/action_figures/#{action_figure.id}"
   end
 
 
   def show
     @action_figure = ActionFigure.find(params[:id])
+  end
+
+  def edit
+    @action_figure = ActionFigure.find(params[:id])
+  end
+
+  def update
+    action_figure = ActionFigure.find(params[:id])
+
+    action_figure.assign_attributes(
+                                    name: params[:name],
+                                    description: params[:description],
+                                    image: params[:image],
+                                    price: params[:price]
+                                    )
+
+    action_figure.save
+    flash[:success] = "Action Figure Updated"
+    redirect_to "/action_figures/#{action_figure.id}"
+  end
+
+  def destroy
+    action_figure = ActionFigure.find(params[:id])
+    action_figure.destroy
+    flash[:success] = "Action Figure Deleted"
+    redirect_to "/action_figures"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   # get '/all_action_figures_url' => 'action_figures#all_action_figures_action'
   get '/' => 'action_figures#index'
   get '/action_figures' => 'action_figures#index'
-
   get '/action_figures/new' => 'action_figures#new'
   post '/action_figures' => 'action_figures#create'
-
   get '/action_figures/:id' => 'action_figures#show'
+  get '/action_figures/:id/edit' => 'action_figures#edit'
+  patch '/action_figures/:id' => 'action_figures#update'
+  delete '/action_figures/:id' => 'action_figures#destroy'
 end
